13fa612b169fdc43bab9925f265b011d82a75bbc
yitznewton <yitznewton@hotmail.com>
2017-11-01 21:57:02 -0400

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,26 +7,31 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  books: Book[] = [];
+  books: BookSummary[] = [];
 
   constructor (private http: HttpClient) {}
 
-  updateQ(event: InputEvent) {
+  updateQ(event): void {
     this.http.get('http://localhost:4567/books', {params: {q: event.target['value']}}).subscribe(books => {
-      this.books = books['results'].map(b => new Book(b));
+      this.books = books['results'].map(b => new BookSummary(b));
     });
   }
 }
 
-class Book {
+class BookSummary {
   readonly id;
   readonly title;
+  readonly author;
   readonly img_src;
+  readonly stars: Number;
+  readonly ratings_count: Number;
 
   constructor (raw_book) {
-    console.info(raw_book);
     this.id = raw_book['best_book']['id'];
     this.title = raw_book['best_book']['title'];
-    this.img_src = raw_book['best_book']['small_image_url'];
+    this.author = raw_book['best_book']['author']['name'];
+    this.img_src = raw_book['best_book']['image_url'];
+    this.stars = Number.parseFloat(raw_book['average_rating']);
+    this.ratings_count = Number.parseFloat(raw_book['ratings_count']);
   }
 }
