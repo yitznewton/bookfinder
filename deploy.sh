e57ef8c0c049b33e6078a44f9fdeeee7996db30f
yitznewton <yitznewton@hotmail.com>
2017-11-02 10:26:49 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ git checkout -b ${BUILD_BRANCH} ${BRANCH_TO_DEPLOY} && \
     ng build -prod && \
     git add -f dist/ && \
     git commit -m "Production build" && \
-    git push -f heroku deploy:master && \
+    git push -f heroku ${BUILD_BRANCH}:master && \
     git checkout - && \
     git branch -D ${BUILD_BRANCH}
