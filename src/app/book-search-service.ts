bd242044d7fcc4261314491907c90de246a91583
yitznewton <yitznewton@hotmail.com>
2017-11-02 09:53:03 -0400

    Extract book search service

---
+++ src/app/book-search-service.ts
@@ -0,0 +1,27 @@
+import { BookSummary } from './book-summary';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Injectable } from '@angular/core';
+
+export abstract class BookSearchService {
+  public abstract get(q: string, page: number): Observable<BookSummary[]>;
+}
+
+@Injectable()
+export class ApiBookSearchService extends BookSearchService {
+  constructor (private http: HttpClient) { super(); }
+
+  public get(q: string, page: number): Observable<BookSummary[]> {
+    return this.http.get('/books', {
+      params: {
+        q: q,
+        page: page.toString()
+      }
+    }).map(response => this.wrap(response));
+  }
+
+  private wrap(response) {
+    return response.results.map(b => new BookSummary(b));
+  }
+}
