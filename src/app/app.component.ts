07dd7e4d203d004b802e89be246e6915c5ed434c
yitznewton <yitznewton@hotmail.com>
2017-11-01 21:14:08 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  books: Book[] = [];
+
+  constructor (private http: HttpClient) {}
+
+  updateQ(event: InputEvent) {
+    this.http.get('http://localhost:4567/books', {params: {q: event.target['value']}}).subscribe(books => {
+      this.books = books['results'].map(b => new Book(b));
+    });
+  }
+}
+
+class Book {
+  readonly id;
+  readonly title;
+  readonly img_src;
+
+  constructor (raw_book) {
+    console.info(raw_book);
+    this.id = raw_book['best_book']['id'];
+    this.title = raw_book['best_book']['title'];
+    this.img_src = raw_book['best_book']['small_image_url'];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
